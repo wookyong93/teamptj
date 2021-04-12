cd80904d1a1a8dd2caaf8917fc3315c4fe122304
wookyong93 <dnrud368@gmail.com>
2021-04-13 00:28:51 +0900

    commit

--- foodchain/src/main/java/com/spring/foodchain/member/DAO/MemberDAOImpl.java
+++ foodchain/src/main/java/com/spring/foodchain/member/DAO/MemberDAOImpl.java
@@ -11,11 +11,12 @@ import com.spring.foodchain.member.VO.MemberVO;
 public class MemberDAOImpl implements MemberDAO{
 	@Autowired
 	private SqlSession sqlSession;
+	
 	@Override
 	public boolean loginCheck(MemberVO memberVO) throws DataAccessException {
 		// TODO Auto-generated method stub
 		
-		boolean result = Boolean.parseBoolean((String)sqlSession.selectOne("mapper.member.loginCheck",memberVO));
+		boolean result = Boolean.parseBoolean((String) sqlSession.selectOne("mapper.member.loginCheck",memberVO));
 		System.out.println(result);
 		return result;
 	}
