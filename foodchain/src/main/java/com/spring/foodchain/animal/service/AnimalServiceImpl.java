0d2d80ba8e7f53f39d5ef133fe73c1c4248b0843
wookyong93 <dnrud368@gmail.com>
2021-04-19 15:07:14 +0900

    error 수정

--- foodchain/src/main/java/com/spring/foodchain/animal/service/AnimalServiceImpl.java
+++ foodchain/src/main/java/com/spring/foodchain/animal/service/AnimalServiceImpl.java
@@ -1,5 +1,9 @@
 package com.spring.foodchain.animal.service;
 
-public class AnimalServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service("animalService")
+//서비스 지정 및 animalService 클레스 상속 처리
+public class AnimalServiceImpl implements AnimalService{
 
 }
