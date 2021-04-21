d743d550ff84d2ffb272621f30a2ad4c3caaaa49
kang <kang@DESKTOP-PT6SE47>
2021-04-21 09:05:15 +0900

    place1

--- foodchain/src/main/java/com/spring/foodchain/place/controller/PlaceController.java
+++ foodchain/src/main/java/com/spring/foodchain/place/controller/PlaceController.java
@@ -1,5 +1,13 @@
 package com.spring.foodchain.place.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 public interface PlaceController {
+
+	public ModelAndView placeRule(@RequestParam("place") String place, HttpServletRequest request, HttpServletResponse response) throws Exception;
 
 }
--- foodchain/src/main/java/com/spring/foodchain/place/controller/PlaceControllerImpl.java
+++ foodchain/src/main/java/com/spring/foodchain/place/controller/PlaceControllerImpl.java
@@ -1,5 +1,25 @@
 package com.spring.foodchain.place.controller;
 
-public class PlaceControllerImpl implements PlaceController{
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.foodchain.place.service.PlaceService;
+
+// 210420 강민경 작성중
+@Controller("placeController")
+public class PlaceControllerImpl implements PlaceController{
+	@Autowired
+	private PlaceService placeService;
+	@Override
+	public ModelAndView placeRule(@RequestParam("place")String place, HttpServletRequest request, HttpServletResponse response)throws Exception{
+		
+		return null;
+		
+	}
 }
